9caf2f4929889daad0b489d15cf59dfe15a2cb43
memo.toro <memo.toro@73dfe3d3-15f0-c740-4bb3-f4d0110f454f>
2012-06-04 01:11:09 +0000

    Cargue de proyecto

---
+++ lab1-anotaciones/src/co/edu/uniandes/banco/anotaciones/TipoNotificacion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.uniandes.banco.anotaciones;
+
+/**
+ *
+ * @author Memo Toro
+ */
+public enum TipoNotificacion {
+    EMAIL,SMS,IM
+}
